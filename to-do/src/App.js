980fb4aec12c0e8e2c0e8824fb07ce1c411ed872
babubalaji877841@gmail.com <babubalaji877841@gmail.com>
2023-12-25 01:16:40 +0530

    commit

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -3,29 +3,21 @@ import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from 'react'
 import './App.css'
+import AddItem from "./AddItem";
 function App() {
-  const [items,setItems]=useState([
-    {
-        id:1,
-        checked:false,
-        item:"Leetcode"
-    },
-    {
-        id:2,
-        checked:false,
-        item : "Internship"
-    },
-    {
-        id:3,
-        checked:false,
-        item:"gym"
-    },
-    {
-        id:4,
-        checked:false,
-        item:"React-Js"
-    }
-]);
+  const [items,setItems]=useState(
+    JSON.parse(localStorage.getItem('todo_list'))
+);
+const [newItem,setNewItem] = useState('')
+
+const addItem=(item)=>{
+  const id=items.length?items[items.length-1].id+1:1;
+  const addNewItem={id,checked:false,item}
+  const listItems=[...items,addNewItem]
+  setItems(listItems)
+  localStorage.setItem("todo_list",JSON.stringify(listItems))
+}
+
 const handleCheck = (id) =>
 {
     const listItems = items.map((item)=>
@@ -33,6 +25,7 @@ const handleCheck = (id) =>
     setItems(listItems)
     localStorage.setItem("todo_list",JSON.stringify(listItems))
 }
+
 const handleDelete = (id) =>
 {
     const listItems = items.filter((item)=>
@@ -40,9 +33,26 @@ const handleDelete = (id) =>
     setItems(listItems)
     localStorage.setItem("todo_list",JSON.stringify(listItems))
 }
+
+const handleSubmit=(e)=>
+{
+  e.preventDefault()
+  console.log("submitted")
+  if(!newItem)
+  return;
+  console.log(newItem)
+  addItem(newItem)
+  setNewItem('')
+}
+
   return (
     <div className="App">
       <Header title="To Do List"/>
+      <AddItem 
+      newItem={newItem}
+      setNewItem={setNewItem}
+      handleSubmit={handleSubmit}
+      />
       <Content 
         items = {items}
         handleCheck={handleCheck}
